4648249e54ef46ce9f68531f251d6eddbb0e589a
yuvraj2017 <yashsavaliya159@gmail.com>
2024-06-10 22:13:04 +0530

    test

--- test.py
+++ test.py
@@ -11,18 +11,26 @@ print("Just for testing")
 
 
 import json
-from datetime import datetime
+from datetime import datetime, date
+from random import randint, randrange
 
 # Get the current date and time
-current_datetime = datetime.now().isoformat()
+# current_datetime = datetime.now().isoformat()
+current_date = date.today()
+
+random_number = randint(0, 100000)
+print("random number", random_number)
 
 # Create a dictionary to store the date and time
 data = {
-    "current_datetime": current_datetime
+    "current_datetime": current_date.isoformat(),
+    "random number": random_number    
 }
 
 # Define the file name
-file_name = "%s.json" % current_datetime
+file_name = "yash.json"
+print(file_name)
+print(type(data))
 
 # Write the date and time to a JSON file
 with open(file_name, 'w') as json_file:
